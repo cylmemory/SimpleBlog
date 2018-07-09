d4fe4f5afe5c0f345f43a43ed4b48d0547d61a89
cylmemory <524545756@qq.com>
2018-07-09 18:18:01 +0800

    add config.py

---
+++ app/config.py
@@ -0,0 +1,48 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fjdljLJDL08_80jflKzcznv*c'
+    MONGODB_SETTINGS = {'DB': 'Tools'}
+
+    TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates').replace('//', '/')
+    STATIC_PATH = os.path.join(BASE_DIR, 'static').replace('//', '/')
+    EXPORT_PATH = os.path.join(BASE_DIR, 'exports').replace('\\', '/')
+
+    if not os.path.exists(EXPORT_PATH):
+        os.makedirs(EXPORT_PATH)
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentCnfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
+    MONGODB_SETTINGS = {
+        'db': os.environ.get('DB_NAME') or 'OctBlog',
+        'host': os.environ.get('MONGO_HOST') or 'localhost',
+        }
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+
+    MONGODB_SETTINGS = {
+        'db': 'ToolsTest',
+        'is_mock': True
+    }
+
+
+config = {
+    'development': DevelopmentCnfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
