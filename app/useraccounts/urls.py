045c9c07b5df8ca09c0bbbcb69715986015ec665
cylmemory <524545756@qq.com>
2018-08-11 13:22:45 +0800

    add users list page

--- app/useraccounts/urls.py
+++ app/useraccounts/urls.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from . import views
-from main import errors
+from ..main import errors
 
 accounts = Blueprint('useraccounts', __name__)
 
@@ -9,5 +9,5 @@ accounts.add_url_rule('/register/', 'register', views.register, methods=['GET', 
 accounts.add_url_rule('register/admin', 'register_admin', views.register, defaults={'admin_create': True},
                       methods=['GET', 'POST'])
 accounts.add_url_rule('/logout/', 'logout', views.logout)
-
+accounts.add_url_rule('/users/', view_func=views.Users.as_view('users'))
 accounts.errorhandler(403)(errors.handle_forbidden)
